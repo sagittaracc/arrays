cb61c8c2e7e79097267bf2d70aa8a2c8421057bc
arutyunyan <arutyunyan@bolid.ru>
2021-06-07 10:37:23 +0300

    add functions index & group

--- src/ArrayHelper.php
+++ src/ArrayHelper.php
@@ -1,33 +1,107 @@
 <?php
+
 namespace sagittaracc;
 
+/**
+ * This is me array helper
+ * I use it for sagittaracc/suql
+ * 
+ * @author sagittaracc <sagittaracc@gmail.com>
+ */
 class ArrayHelper
 {
-  public static function slice_by_keys($array, $keys)
-  {
-    if (!is_array($array) || !is_array($keys))
-      return null;
-
-    if (empty($keys))
-      return [];
-
-    return array_intersect_key($array, array_fill_keys($keys, 0));
-  }
-
-  public static function rename_keys($array, $new_keys)
-  {
-    $old_keys = array_keys($array);
-
-    if (count($old_keys) !== count($new_keys))
+    /**
+     * Строит срез массива по набору ключей
+     * @param array $array
+     * @param array $key
+     * @return array
+     */
+    public static function slice_by_keys($array, $keys)
     {
-      return null;
+        if (!is_array($array) || !is_array($keys))
+            return null;
+
+        if (empty($keys))
+            return [];
+
+        return array_intersect_key($array, array_fill_keys($keys, 0));
     }
+    /**
+     * Переиндексирует исходный массив новым набором ключей
+     * @param array $array
+     * @param array $new_keys
+     * @return array
+     */
+    public static function rename_keys($array, $new_keys)
+    {
+        $old_keys = array_keys($array);
 
-    return array_combine($new_keys, array_values($array));
-  }
+        if (count($old_keys) !== count($new_keys)) {
+            return null;
+        }
 
-  public static function isSequential($arr)
-  {
-    return array_keys($arr) === range(0, count($arr) - 1);
-  }
+        return array_combine($new_keys, array_values($array));
+    }
+    /**
+     * Проверяет если исходный массив is sequential
+     * @param array $array
+     * @return boolean
+     */
+    public static function isSequential($array)
+    {
+        return array_keys($array) === range(0, count($array) - 1);
+    }
+    /**
+     * Индексирует входной массив по набору ключей
+     * @param string|array $option
+     * @param array $array
+     * @param boolean $group признак группировки или индексации
+     * @return array
+     */
+    public static function index($option, $array, $group = false)
+    {
+        $indexArr = [];
+        $pointer = null;
+
+        if (is_string($option)) {
+            $simpleIndex = $option;
+
+            foreach ($array as $row) {
+                $group
+                    ? $indexArr[$row[$simpleIndex]][] = $row
+                    : $indexArr[$row[$simpleIndex]] = $row;
+            }
+        }
+        else if (is_array($option)) {
+            $multiIndex = $option;
+
+            foreach ($array as $row) {
+                $pointer = &$indexArr;
+
+                foreach ($multiIndex as $index) {
+                    if (!isset($pointer[$row[$index]])) {
+                        $pointer[$row[$index]] = [];
+                    }
+
+                    $pointer = &$pointer[$row[$index]];
+                }
+
+                $group
+                    ? $pointer[] = $row
+                    : $pointer = $row;
+            }
+        }
+
+        return $indexArr;
+    }
+    /**
+     * Группировка исходного массива по набору ключей
+     * @param string|array $option
+     * @param array $array
+     * @return array
+     */
+    public static function group($option, $array)
+    {
+        return self::index($option, $array, true);
+    }
 }
--- tests/ArrayTest.php
+++ tests/ArrayTest.php
@@ -1,42 +1,108 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 use sagittaracc\ArrayHelper;
 
 final class ArrayTest extends TestCase
 {
-  public function testArrays(): void
-  {
-    $this->assertEquals(ArrayHelper::slice_by_keys([
-      'e1' => 'v1',
-      'e2' => 'v2',
-      'e3' => 'v3',
-    ], ['e1', 'e3']), [
-      'e1' => 'v1',
-      'e3' => 'v3',
-    ]);
+    public function testArrays(): void
+    {
+        $this->assertEquals(ArrayHelper::slice_by_keys([
+            'e1' => 'v1',
+            'e2' => 'v2',
+            'e3' => 'v3',
+        ], ['e1', 'e3']), [
+            'e1' => 'v1',
+            'e3' => 'v3',
+        ]);
 
-    $this->assertTrue(ArrayHelper::isSequential([1,2,3]));
-    $this->assertFalse(ArrayHelper::isSequential(['foo' => 'bar']));
+        $this->assertTrue(ArrayHelper::isSequential([1, 2, 3]));
+        $this->assertFalse(ArrayHelper::isSequential(['foo' => 'bar']));
 
-    $this->assertEquals(ArrayHelper::rename_keys([
-      'old_key1' => 1,
-      'old_key2' => 2,
-      'old_key3' => 3,
-    ], [
-      'new_key1', 'new_key2', 'new_key3'
-    ]), [
-      'new_key1' => 1,
-      'new_key2' => 2,
-      'new_key3' => 3,
-    ]);
+        $this->assertEquals(ArrayHelper::rename_keys([
+            'old_key1' => 1,
+            'old_key2' => 2,
+            'old_key3' => 3,
+        ], [
+            'new_key1', 'new_key2', 'new_key3'
+        ]), [
+            'new_key1' => 1,
+            'new_key2' => 2,
+            'new_key3' => 3,
+        ]);
 
-    $this->assertNull(ArrayHelper::rename_keys([
-      'old_key1' => 1,
-      'old_key2' => 2,
-      'old_key3' => 3,
-    ], [
-      'new_key1', 'new_key2', 'new_key3', 'new_key4'
-    ]));
-  }
+        $this->assertNull(ArrayHelper::rename_keys([
+            'old_key1' => 1,
+            'old_key2' => 2,
+            'old_key3' => 3,
+        ], [
+            'new_key1', 'new_key2', 'new_key3', 'new_key4'
+        ]));
+
+        $this->assertEquals(ArrayHelper::index('id', [
+            ['id' => 1, 'text' => 'foo'],
+            ['id' => 2, 'text' => 'bar'],
+        ]), [
+            1 => [
+                'id' => 1,
+                'text' => 'foo'
+            ],
+            2 => [
+                'id' => 2,
+                'text' => 'bar'
+            ],
+        ]);
+
+        $this->assertEquals(ArrayHelper::index(['id', 'text'], [
+            ['id' => 1, 'text' => 'foo'],
+            ['id' => 2, 'text' => 'bar'],
+        ]), [
+            1 => [
+                'foo' => [
+                    'id' => 1,
+                    'text' => 'foo',
+                ],
+            ],
+            2 => [
+                'bar' => [
+                    'id' => 2,
+                    'text' => 'bar',
+                ],
+            ],
+        ]);
+
+        $this->assertEquals(ArrayHelper::group('text', [
+            ['id' => 1, 'text' => 'foo'],
+            ['id' => 2, 'text' => 'bar'],
+            ['id' => 3, 'text' => 'bar'],
+        ]), [
+            'foo' => [
+                ['id' => 1, 'text' => 'foo']
+            ],
+            'bar' => [
+                ['id' => 2, 'text' => 'bar'],
+                ['id' => 3, 'text' => 'bar'],
+            ],
+        ]);
+
+        $this->assertEquals(ArrayHelper::group(['text', 'data'], [
+            ['id' => 1, 'text' => 'foo', 'data' => 'input'],
+            ['id' => 2, 'text' => 'bar', 'data' => 'output'],
+            ['id' => 3, 'text' => 'bar', 'data' => 'output'],
+        ]), [
+            'foo' => [
+                'input' => [
+                    ['id' => 1, 'text' => 'foo', 'data' => 'input'],
+                ]
+            ],
+            'bar' => [
+                'output' => [
+                    ['id' => 2, 'text' => 'bar', 'data' => 'output'],
+                    ['id' => 3, 'text' => 'bar', 'data' => 'output'],
+                ]
+            ],
+        ]);
+    }
 }
